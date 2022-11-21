f427f50af95aec6172cf2bf381765bffd58d78ff
Jonas Kruckenberg <iterpre@protonmail.com>
2022-11-21 16:27:58 +0100

    Update chars.rs

--- crates/gen-host/tests/chars.rs
+++ crates/gen-host/tests/chars.rs
@@ -1,10 +1,11 @@
 #[allow(clippy::all)]
 pub mod imports {
+    #[::tauri_bindgen_host::async_trait]
     pub trait Imports: Sized {
         /// A function that accepts a character
-        fn take_char(&self, x: char) -> ::tauri_bindgen_host::anyhow::Result<()>;
+        async fn take_char(&self, x: char) -> ::tauri_bindgen_host::anyhow::Result<()>;
         /// A function that returns a character
-        fn return_char(&self) -> ::tauri_bindgen_host::anyhow::Result<char>;
+        async fn return_char(&self) -> ::tauri_bindgen_host::anyhow::Result<char>;
     }
 
     pub fn invoke_handler<U, R>(ctx: U) -> impl Fn(::tauri_bindgen_host::tauri::Invoke<R>)
@@ -20,21 +21,24 @@ pub mod imports {
                     resolver: __tauri_resolver__,
                 } = invoke;
 
-                let result = ctx.take_char(
-                    match ::tauri_bindgen_host::tauri::command::CommandArg::from_command(
-                        ::tauri_bindgen_host::tauri::command::CommandItem {
-                            name: "take-char",
-                            key: "x",
-                            message: &__tauri_message__,
+                __tauri_resolver__.respond_async_serialized(async move {
+                    let result = ctx.take_char(
+                        match ::tauri_bindgen_host::tauri::command::CommandArg::from_command(
+                            ::tauri_bindgen_host::tauri::command::CommandItem {
+                                name: "take-char",
+                                key: "x",
+                                message: &__tauri_message__,
+                            },
+                        ) {
+                            Ok(arg) => arg,
+                            Err(err) => return __tauri_resolver__.invoke_error(err),
                         },
-                    ) {
-                        Ok(arg) => arg,
-                        Err(err) => return __tauri_resolver__.invoke_error(err),
-                    },
-                );
+                    );
 
-                __tauri_resolver__
-                    .respond(result.map_err(::tauri_bindgen_host::tauri::InvokeError::from_anyhow));
+                    result
+                        .await
+                        .map_err(::tauri_bindgen_host::tauri::InvokeError::from_anyhow)
+                });
             }
             "return-char" => {
                 #[allow(unused_variables)]
@@ -43,10 +47,13 @@ pub mod imports {
                     resolver: __tauri_resolver__,
                 } = invoke;
 
-                let result = ctx.return_char();
+                __tauri_resolver__.respond_async_serialized(async move {
+                    let result = ctx.return_char();
 
-                __tauri_resolver__
-                    .respond(result.map_err(::tauri_bindgen_host::tauri::InvokeError::from_anyhow));
+                    result
+                        .await
+                        .map_err(::tauri_bindgen_host::tauri::InvokeError::from_anyhow)
+                });
             }
             _ => invoke.resolver.reject("Not Found"),
         }
