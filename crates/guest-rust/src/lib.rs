81163f55e75bd7ee42f4dac1b0bb678cebc6bf59
Jonas Kruckenberg <iterpre@protonmail.com>
2022-11-21 14:18:03 +0100

    chores

--- crates/guest-rust/src/lib.rs
+++ crates/guest-rust/src/lib.rs
@@ -1,6 +1,7 @@
 // #[cfg(feature = "macros")]
 pub use guest_rust_macro::*;
 
+
 // Re-export `bitflags` so that we can reference it from macros.
 #[doc(hidden)]
-pub use {bitflags, serde, serde_wasm_bindgen, wasm_bindgen};
+pub use {bitflags, serde_wasm_bindgen, wasm_bindgen};
