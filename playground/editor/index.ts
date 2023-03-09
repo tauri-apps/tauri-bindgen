8fa4b8d0098d7d357db1931c5fae98b6ceda057a
Jonas Kruckenberg <iterpre@protonmail.com>
2023-03-09 20:52:10 +0100

    feat: add playground

---
+++ playground/editor/index.ts
@@ -0,0 +1,62 @@
+import { EditorView, basicSetup } from "codemirror"
+import { javascript } from "@codemirror/lang-javascript"
+import { rust } from "@codemirror/lang-rust"
+import { EditorState } from "@codemirror/state"
+
+const default_content = `interface greet {
+    func greet(name: string) -> string
+}`
+
+const outputs = {
+    'errors': new EditorView({
+        parent: document.getElementById('errors')!,
+        extensions: [basicSetup, javascript(), EditorState.readOnly.of(true)],
+    }),
+    'host': new EditorView({
+        parent: document.getElementById('host')!,
+        extensions: [basicSetup, rust(), EditorState.readOnly.of(true)],
+    }),
+    'guest-rust': new EditorView({
+        parent: document.getElementById('guest_rust')!,
+        extensions: [basicSetup, rust(), EditorState.readOnly.of(true)],
+    }),
+    'guest-js': new EditorView({
+        parent: document.getElementById('guest_js')!,
+        extensions: [basicSetup, javascript(), EditorState.readOnly.of(true)],
+    }),
+    'guest-ts': new EditorView({
+        parent: document.getElementById('guest_ts')!,
+        extensions: [basicSetup, javascript(), EditorState.readOnly.of(true)],
+    }),
+}
+
+export function updateOutput(id: string, data: string) {
+    outputs[id].dispatch({
+        changes: {
+            from: 0,
+            to: outputs[id].state.doc.length,
+            insert: data,
+        },
+    })
+}
+
+export function setup(onChange: (data: string) => void) {
+    const input = new EditorView({
+        parent: document.getElementById('input')!,
+        extensions: [basicSetup, EditorView.updateListener.of((v) => {
+            if (v.docChanged) {
+                console.log(v.state.doc.toString())
+
+                onChange(v.state.doc.toString())
+            }
+        })],
+    })
+
+    input.dispatch({
+        changes: {
+            from: 0,
+            to: input.state.doc.length,
+            insert: default_content,
+        },  
+    })
+}
---
+++ playground/src/main.rs
@@ -0,0 +1,89 @@
+use miette::NamedSource;
+use tauri_bindgen_core::GeneratorBuilder;
+use wasm_bindgen::prelude::*;
+use wit_parser::Interface;
+
+#[wasm_bindgen(module = "/editor/dist/index.mjs")]
+extern "C" {
+    fn setup(on_change: &Closure<dyn FnMut(JsValue)>);
+    #[wasm_bindgen(js_name = "updateOutput")]
+    fn update_output(id: &str, value: &str);
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
+
+    let on_change = Closure::wrap(Box::new(|value: JsValue| {
+        let source = value.as_string().unwrap();
+
+        let parse_res = wit_parser::parse_str(&source, |_| false)
+            .map_err(|err| err.with_source_code(NamedSource::new("input", source)));
+
+        log::debug!("value: {:?}", parse_res);
+
+        match parse_res {
+            Ok(iface) => {
+                update_output("errors", "");
+                update_output(
+                    "guest-rust",
+                    &gen_interface(
+                        tauri_bindgen_gen_guest_rust::Builder {
+                            fmt: true,
+                            unchecked: false,
+                            no_std: false,
+                        },
+                        iface.clone(),
+                    ),
+                );
+                update_output(
+                    "host",
+                    &gen_interface(
+                        tauri_bindgen_gen_host::Builder {
+                            fmt: true,
+                            tracing: false,
+                            async_: false,
+                        },
+                        iface.clone(),
+                    ),
+                );
+                update_output(
+                    "guest-js",
+                    &gen_interface(
+                        tauri_bindgen_gen_guest_js::Builder {
+                            prettier: false,
+                            romefmt: false,
+                        },
+                        iface.clone(),
+                    ),
+                );
+                update_output(
+                    "guest-ts",
+                    &gen_interface(
+                        tauri_bindgen_gen_guest_ts::Builder {
+                            prettier: false,
+                            romefmt: false,
+                        },
+                        iface,
+                    ),
+                );
+            }
+            Err(err) => {
+                update_output("errors", &format!("{:?}", err));
+            }
+        }
+    }) as Box<dyn FnMut(JsValue)>);
+
+    setup(&on_change);
+
+    on_change.forget();
+}
+
+fn gen_interface<B>(builder: B, iface: Interface) -> String
+where
+    B: GeneratorBuilder,
+{
+    let gen = builder.build(iface);
+
+    gen.to_file().1
+}
