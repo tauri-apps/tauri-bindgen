2b8bef5961920d73fb53ae76149b0ca87c99b47e
Jonas Kruckenberg <iterpre@protonmail.com>
2023-09-10 14:30:27 +0200

    Update resources.rs

--- crates/gen-guest-rust/tests/resources.rs
+++ crates/gen-guest-rust/tests/resources.rs
@@ -7,17 +7,29 @@ pub mod resources {
     pub struct A(u32);
     impl A {
         pub async fn f1(&self) {
-            ::tauri_bindgen_guest_rust::invoke("resources;::resource::A", "f1", &())
+            ::tauri_bindgen_guest_rust::invoke(
+                    "resources::resource::a",
+                    "f1",
+                    &(self.0,),
+                )
                 .await
                 .unwrap()
         }
         pub async fn f2(&self, a: u32) {
-            ::tauri_bindgen_guest_rust::invoke("resources;::resource::A", "f2", &(a))
+            ::tauri_bindgen_guest_rust::invoke(
+                    "resources::resource::a",
+                    "f2",
+                    &(self.0, a),
+                )
                 .await
                 .unwrap()
         }
         pub async fn f3(&self, a: u32, b: u32) {
-            ::tauri_bindgen_guest_rust::invoke("resources;::resource::A", "f3", &(a, b))
+            ::tauri_bindgen_guest_rust::invoke(
+                    "resources::resource::a",
+                    "f3",
+                    &(self.0, a, b),
+                )
                 .await
                 .unwrap()
         }
@@ -26,17 +38,29 @@ pub mod resources {
     pub struct B(u32);
     impl B {
         pub async fn f1(&self) -> A {
-            ::tauri_bindgen_guest_rust::invoke("resources;::resource::B", "f1", &())
+            ::tauri_bindgen_guest_rust::invoke(
+                    "resources::resource::b",
+                    "f1",
+                    &(self.0,),
+                )
                 .await
                 .unwrap()
         }
         pub async fn f2(&self, x: A) -> Result<u32, ()> {
-            ::tauri_bindgen_guest_rust::invoke("resources;::resource::B", "f2", &(x))
+            ::tauri_bindgen_guest_rust::invoke(
+                    "resources::resource::b",
+                    "f2",
+                    &(self.0, x),
+                )
                 .await
                 .unwrap()
         }
         pub async fn f3(&self, x: Option<&'_ [A]>) -> Result<A, ()> {
-            ::tauri_bindgen_guest_rust::invoke("resources;::resource::B", "f3", &(x))
+            ::tauri_bindgen_guest_rust::invoke(
+                    "resources::resource::b",
+                    "f3",
+                    &(self.0, x),
+                )
                 .await
                 .unwrap()
         }
